3b28fe65793fe256415a0699eac0cb7d8b5fceb2
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-01-09 22:35:05 +0800

    💄 Added proof of concept product page
    created a layout to test the static product effectt

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex h-dvh w-full items-center justify-center">
+    <div className="bg-mango-300 flex h-dvh w-full items-center justify-center">
       <h1 className="text-4xl font-extrabold">
         <span className="text-xl font-bold">Home of</span>
         <br /> <Link to="test">Guadalupe Dried Mangoes</Link>
