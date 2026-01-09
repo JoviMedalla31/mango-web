19e2cff5fb5d398ffe7e4beabd6e7f876db94894
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-01-09 16:23:40 +0800

    💄 Added stand-in home page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <>Home Page</>;
+  return (
+    <div className="flex w-full h-dvh items-center justify-center">
+      <h1 className="text-4xl font-extrabold">
+        <span className="text-xl font-bold">Home of</span>
+        <br /> Guadalupe Dried Mangoes
+      </h1>
+    </div>
+  );
 }
 
 export default App;
