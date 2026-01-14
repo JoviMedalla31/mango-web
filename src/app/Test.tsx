11aa7134b95c83c64d1ada966b8c42157a00709e
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-01-14 22:04:33 +0800

    💄 Added 3 column layout in products page

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -2,59 +2,7 @@ import mangoes from '@/assets/mangoes.jpg';
 
 function Test() {
   console.log(import.meta.env.BASE_URL);
-  return (
-    <div className="bg-mango-100">
-      <header className="bg-mango-400 h-16 w-full"></header>
-      <main className="flex flex-col items-center justify-center pt-20">
-        <div className="pointer-events-none fixed inset-x-auto top-30 z-1 w-full max-w-254 px-8 select-none">
-          <div className="mx-auto w-1/3">
-            <img className="" src={mangoes} />
-          </div>
-        </div>
-        <div className="relative grid w-full max-w-254 grid-cols-3">
-          <div className="text-mango-400 relative">
-            <div className="mx-auto flex w-fit flex-col">
-              <h1 className="text-4xl/tight font-semibold">
-                Dried <br />
-                Mango
-              </h1>
-              <h1 className="text-6xl font-bold text-black">STRIPS</h1>
-              <h3 className="text-right text-3xl">100g</h3>
-            </div>
-          </div>
-          <div className="row-span-2" />
-          <div className="px-6 pt-30 pb-42">
-            <p className="text-center text-xl/snug">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div className="px-6 pb-16">
-            <p className="text-center text-xl/snug">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div className="px-6 pt-40 pb-16">
-            <p className="text-center text-xl/snug">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div className="h-1000 px-6 pt-50 pb-16">
-            <p className="text-center text-xl/snug">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+  return <div className="bg-mango-100">This is a test</div>;
 }
 
 export default Test;
--- src/app/routes/Product.tsx
+++ src/app/routes/Product.tsx
@@ -1,18 +1,47 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Papa from 'papaparse';
-
-import logo from '/images/logo.png';
-import strips from '@/assets/products/strips.csv?raw';
 import { Link } from 'react-router-dom';
+
+import logo from '/images/logo.svg';
+import strips from '@/assets/products/strips.csv?raw';
+import stripsImg from '/images/strips/product.png';
+import ingredientsTop from '/images/products/ingredients-top.svg';
+import footerA from '/images/footer/footer-a.svg';
+import footerB from '/images/footer/footer-b.svg';
+import { Menu } from 'lucide-react';
+
+const Header = () => {
+  return (
+    <header className="flex w-full flex-col items-center">
+      <nav className="poppins bg-mango-400 flex h-12 w-full items-center justify-center text-xl text-white">
+        <div className="hidden items-center justify-center gap-8 sm:flex">
+          <p>About Us</p>
+          <p>Products</p>
+          <p>How to Order?</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="flex w-full items-center justify-between px-6 sm:hidden">
+          <p className="font-semibold">Guadalupe</p>
+          <Menu className="" />
+        </div>
+      </nav>
+      <div className="hidden w-full max-w-300 -translate-y-0.5 sm:block">
+        <Link to="/">
+          <img src={logo} className="ml-28" />
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 const Products = () => {
   const { product } = useParams();
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<string[][]>([]);
 
   useEffect(() => {
     const parseData = async () => {
-      let nextData = await Papa.parse(strips);
+      let { data: nextData }: { data: string[][] } = await Papa.parse(strips);
       setData(nextData);
 
       console.log(nextData);
@@ -21,28 +50,86 @@ const Products = () => {
     parseData();
   }, []);
 
-  console.log('image url: ', `/images/${data?.data?.[0][0]}`);
+  // console.log('image url: ', `/images/${data?.data?.[0][0]}`);
 
   return (
-    <div className="bg-mango-100 min-h-dvh w-full">
-      <header className="flex w-full flex-col items-center">
-        <nav className="poppins bg-mango-400 flex h-12 w-full items-center justify-center gap-10 text-xl text-white">
-          <p>About Us</p>
-          <p>Products</p>
-          <p>How to Order?</p>
-          <p>Contact Us</p>
-        </nav>
-        <div className="w-full max-w-300">
-          <Link to="/">
-            <img src={logo} className="ml-28" />
-          </Link>
+    <div className="bg-mango-100 text-mango-800 flex min-h-dvh w-full flex-col items-center">
+      <Header />
+      <main className="font-poppins mt-8 mb-60 w-full items-center px-4 text-2xl">
+        <div className="pointer-events-none fixed left-1/2 w-full max-w-300 -translate-x-1/2">
+          <div className="mx-auto w-1/3 px-2">
+            <img src={stripsImg} />
+          </div>
         </div>
-      </header>
-      <div className="mt-5 flex flex-col items-center">
-        <img src={import.meta.env.BASE_URL + `images/${data?.data?.[0][0]}`} />
-        <h3 className="font-pt-serif text-mango-800 mt-10 text-4xl font-bold">{data?.data?.[0][1]}</h3>
-        <p className="font-poppins text-mango-800 mt-3 max-w-78 text-center text-2xl">{data?.data?.[0][2]}</p>
-      </div>
+        <div className="mx-auto w-full max-w-300">
+          <div className="grid w-full auto-rows-auto grid-cols-3 gap-8">
+            <div className="relative col-start-2 row-span-full"> {/* This div will create empty space between the grid*/}</div>
+            <div className="col-start-1">
+              <div className="text-mango-400 mx-auto mt-18 flex w-fit flex-col items-start">
+                <h1 className="text-5xl">
+                  Dried Mango <br />
+                  <span className="text-mango-800 text-8xl font-extrabold">STRIPS</span>
+                </h1>
+                <h2 className="self-end text-right text-5xl">100g</h2>
+              </div>
+            </div>
+            <div className="col-start-3 pt-6 text-center">
+              We make use of all parts of the mango, this product is made from the mango surrounding the seeds to create a unique product
+              that taste the same as the Original Slices
+            </div>
+            {data.map((block, i) => (
+              <div className={`${i % 2 == 0 ? 'col-start-1 mt-60' : 'col-start-3'} flex flex-col items-center`}>
+                <img src={import.meta.env.BASE_URL + `images/${block[0]}`} />
+                <h3 className={`font-pt-serif mt-8 text-4xl font-bold`}>{block[1]}</h3>
+                <p className="mt-3 text-center text-2xl">{block[2]}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="z-1 flex w-full flex-col items-center">
+        <div className="z-1 col-span-3 col-start-1 grid max-w-300 grid-cols-3 gap-x-8">
+          <div className="col-start-1 px-8 text-center">
+            <div>
+              <img src={ingredientsTop} className="-mb-0.5 w-full" />
+              <div className="bg-mango-400 w-full px-6 pt-8 pb-24">
+                <h3 className="font-pt-serif text-4xl font-bold">Ingredients</h3>
+                <p className="mt-4 text-2xl">
+                  Selected ripe mangoes, refined sugar, sweeteners (glucose & sorbitol), iodized salt, sodium metabisulfate as perservative
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-start-3">
+            <div className="border-faded-mango-200 rounded-3xl border-4 border-dashed px-8 py-4 text-center">
+              <h3 className="font-pt-serif text-4xl font-bold">Ready to order?</h3>
+              <p className="mt-4">click the link below to see how you can order</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-faded-mango-200 relative -mt-20 h-20 w-full">
+          <svg
+            preserveAspectRatio="none"
+            height="51"
+            viewBox="0 0 1440 51"
+            xmlns="http://www.w3.org/2000/svg"
+            className="fill-faded-mango-200 w-full -translate-y-[98%]"
+          >
+            <path d="M431.76 21.8398L648.48 45.3604L863.52 16.7998L1080.24 40.3203L1295.28 38.6406L1440 24.0557V56.5H0V30.2402L216.72 0L431.76 21.8398Z" />
+          </svg>
+        </div>
+        <div className="bg-mango-400 z-2 h-20 w-full">
+          <svg
+            preserveAspectRatio="none"
+            height="51"
+            viewBox="0 0 1440 51"
+            xmlns="http://www.w3.org/2000/svg"
+            className="fill-mango-400 w-full -translate-y-[98%]"
+          >
+            <path d="M1440 51H0V14.5742L216.72 33.0537L431.76 7.85352L648.48 16.2539L863.521 33.0537L1080.24 43.1338L1295.28 7.85352L1440 0V51Z" />
+          </svg>
+        </div>
+      </footer>
     </div>
   );
 };
