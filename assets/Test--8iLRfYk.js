45970ba4e773fb9e3c2c8af0d2980dafe60cc87a
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-02-07 15:53:40 +0800

    Updates

--- assets/Test--8iLRfYk.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BP-_-hn7.js";function o(){return console.log("/mango-web/"),s.jsx("div",{className:"bg-mango-100",children:"This is a test"})}export{o as default};
---
+++ assets/Test-D2YwG2xg.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BqXLueZf.js";function o(){return console.log("/mango-web/"),s.jsx("div",{className:"bg-mango-100",children:"This is a test"})}export{o as default};
