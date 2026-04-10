e3cab95aab52994c33d78096adda587d82cd2041
jmrvicencio <35318735+jmrvicencio@users.noreply.github.com>
2026-04-10 19:28:07 +0800

    Update: Vite Config base URL

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mango-web/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
